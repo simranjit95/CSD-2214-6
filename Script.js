a6137acc97adf8c1228ce46e5a42e17b1d7c06b1
simranjit95 <46692954+simranjit95@users.noreply.github.com>
2019-01-14 17:08:02 -0500

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,15 @@
+// JavaScript source code
+//Name:Simranjit Kaur
+//Date: January 14,2019
+//Student:c0743554
+//Course: CSD 2214 Section 6
+let countDown;
+let timeInterval;
+const endDate = document.querySelector('input[name="endDate]');
+const clock = document.querySelector('#clock');
+const daysSpan = clock.querySelector('.days');
+const hoursSpan = clock.querySelector('.hours');
+const minutesSpan = clock.querySelector('.minutes');
+const secondsSpan = clock.querySelector('.seconds');
+//console.log("heyyyyy");
+//console.log(endDate.value);
